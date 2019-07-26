f960e08fcc6551691cfdad307a6051a98fcb1fff
YanMao186 <18629281909@163.com>
2019-07-26 23:22:16 +0800

    修复bug

--- zikkinserver/src/main/java/com/ym/ZipkinApplication.java
+++ zikkinserver/src/main/java/com/ym/ZipkinApplication.java
@@ -2,10 +2,12 @@ package com.ym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+//@EnableEurekaClient
 //启用zipkinServer
 public class ZipkinApplication {
     public static void main(String[] args) {
